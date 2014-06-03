a632c9a71f6647bf2c1c020d067650df6a39dd59
ukyoda <black12172001@hotmail.co.jp>
2014-06-04 02:28:29 +0900

    画面サイズを可変にに変更した際に，プレイヤー削除機能が動作しなくなっていましたので修正しました

--- assets/removePlayer.js
+++ assets/removePlayer.js
@@ -1,6 +1,6 @@
 
 Game.fn.removePlayer = function(id) {
-	this.stage.removeChild(this.players[id]);
+	this.playerLayer.removeChild(this.players[id]);
 	this.players[id] = undefined;
 	if(this.player1.id === id) {
 		this.player1 = undefined;
