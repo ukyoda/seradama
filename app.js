d838cc6996ef68408aadb35a7281675c6aa9920a
muff1225 <graphil1225@yahoo.co.jp>
2014-06-08 00:08:34 +0900

    TiwitterOauthライブラリ追加
    依存度のあるLib入れてるんでnpm install打ってね

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var OAuth = require('oauth').OAuth
+var session = require('express-session');
+var passport = require('passport')
+var TwitterStrategy = require('passport-twitter').Strategy
+
+var router = express.Router();
 
 //ゲーム管理変数
 var gameInfoData = {
@@ -35,6 +41,77 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/field.json', api.field);
+
+
+app.use(cookieParser('gravity'));
+app.use(session({secret: "gravity"}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//passportLibのセッション管理用関数（らしい）
+passport.serializeUser(function(user, done){
+  done(null, user);
+});
+ 
+passport.deserializeUser(function(obj, done){
+  done(null, obj);
+});
+
+//ここからTwitter認証の記述
+var TWITTER_CONSUMER_KEY = "consumer_Keyだよ";
+var TWITTER_CONSUMER_SECRET = "Secretキーだよ";
+// TwitterStrategyオブジェクト内に必要な情報を詰める。
+passport.use(new TwitterStrategy({
+  consumerKey: TWITTER_CONSUMER_KEY,
+  consumerSecret: TWITTER_CONSUMER_SECRET,
+  callbackURL: "http://127.0.0.1/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    passport.session.accessToken = token;
+    passport.session.profile = profile;
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
+
+// app.get('/account/twitter', twitterEnsureAuthenticated, routes.account);
+// function twitterEnsureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) { return next(); }
+//   res.redirect('/login/twitter');
+// };
+
+// app.get('/login/twitter', routes.login_facebook);
+
+// app.get('/', function(req, res){
+//     // console.log(req.session.passport.user.username);
+//     // console.log(req.session.passport.user._json.profile_image_url);
+//     // game 処理
+//     /* GET home page. */
+//     router.get('/', function(req, res) {
+//       res.render('index', { title: 'Express' });
+//     });
+// });
+
+//Twitter Authのroute
+app.get('/auth/twitter',
+  passport.authenticate('twitter'),
+  function(req, res){}
+);
+
+//Twitter Authの完了時のRoute
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login/twitter' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+// とりあえずログアウトも
+app.get('/logout/twitter', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
