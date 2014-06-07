324973de9e535f0d0d48dfd76902b439daab04db
ukyoda <black12172001@hotmail.co.jp>
2014-06-07 12:30:22 +0900

    縦横サイズをフ3:2に指定

--- public/javascripts/dist/srdm.js
+++ public/javascripts/dist/srdm.js
@@ -437,22 +437,26 @@ Game.fn.animate = function(){
 	//ウインドウサイズ取得
 	var windowWidth = $(window).width();
 	var windowHeight = $(window).height();
-	var size, scale;
+	var size={x:1,y:1},scale={x:1,y:1};
 	if(windowWidth < windowHeight) {
-		size = windowWidth;
+		size.x = windowWidth;
+		size.y = size.x / 2 * 3;
 	} else {
-		size = windowHeight;
+		size.y = windowHeight;
+		size.x = size.y / 3 * 2;
 	}
-	size *= 0.8;	//0.8倍にする
-	scale = size/800;
-	size = 800*scale;
+	size.x *= 0.8;	//0.8倍にする
+	size.y *= 0.8;
+	scale.x = size.x/this.worldSize.width;
+	scale.y = size.y/this.worldSize.height;
+
 	//ウインドウサイズリサイズ
-	this.renderer.resize(size, size);
-	this.$el.width(size).height(size);
+	this.renderer.resize(size.x, size.y);
+	this.$el.width(size.x).height(size.y);
 	//スケール変換
-	this.playerLayer.scale.set(scale,scale);
-	this.fieldLayer.scale.set(scale, scale);
-	this.objectLayer.scale.set(scale, scale);
+	this.playerLayer.scale.set(scale.x, scale.y);
+	this.fieldLayer.scale.set(scale.x, scale.y);
+	this.objectLayer.scale.set(scale.x, scale.y);
 
 	this.renderer.render(this.stage);
 	window.requestAnimFrame(function(){
