7ba0bc97f974b627c17d29b330768f79eb70ca76
muff1225 <graphil1225@yahoo.co.jp>
2014-06-11 00:21:36 +0900

    tweetとiine!

--- public/javascripts/top.js
+++ public/javascripts/top.js
@@ -3,8 +3,13 @@
 	},
 	init:function(){
 		$(function(){
-			$('.login').click(function(e) {
-				
+			$('.start').click(function(e) {
+				$('.login-page').show("slow");
+				return false;
+			});
+			$('.fa-times').click(function(e) {
+				$('.login-page').hide("slow");
+				return false;
 			});
 		});
 	}
