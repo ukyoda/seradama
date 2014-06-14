0aae051d9a8d57691bdb1a19ce7553c646f881d2
ukyoda <black12172001@hotmail.co.jp>
2014-06-14 15:16:59 +0900

    画像読み込みしてみる

--- assets/updatePlayer.js
+++ assets/updatePlayer.js
@@ -1,11 +1,14 @@
 
 Game.fn.updatePlayer = function(data){
 	var id = data.id;
+	var name = data.name;
 	var position = {
 		x:window.parseFloat(data.x, 10),
 		y:window.parseFloat(data.y, 10)
 	};
 	var textureId = data.texture;
+	var picture = data.picture;
+	var userType = data.userType || "guest";
 	var datatype = data.datatype || "object";
 	var angle = window.parseFloat(data.angle, 10);
 	var layer = this.playerLayer, filterVal=[], sprite;
@@ -15,33 +18,27 @@ Game.fn.updatePlayer = function(data){
 		return null;
 	}
 
-	if(layer.hash[id]) {
-		sprite = layer.hash[id];
-	} else {
-		sprite = new PIXI.Sprite.fromFrame(textureId);
-		sprite.id = id;	//固有ID記憶
+	sprite = (function(){
+		if(layer.hash[id]) {
+			return layer.hash[id];
+		}
+		if(userType === "guest") {
+			sprite = new PIXI.Sprite.fromFrame(textureId);
+		} else {
+			sprite = new PIXI.Sprite.fromImage(picture);
+		}
+		sprite.name = name;
+		sprite.id = id;
 		layer.addChild(sprite);
-		layer.hash[id] = sprite; //ハッシュに登録
+		layer.hash[id] = sprite;
 		sprite.width=32;sprite.height=32;
-	}
+		return sprite;
+	}());
 
 	//スプライト情報を更新
 	sprite.position.set(position.x,position.y);
 	sprite.anchor.set(0.5,0.5);
 	sprite.rotation = angle;
-
-	//あたり判定(自分のボールのみ)
-//	if(
-//		this.player &&
-//		this.player.id === id &&
-//		data.collision) {
-//		if(this.player.collision) {
-//			this.audio.atack.stop().play();
-//			this.player.collision = false;
-//		} else {
-//			this.player.collision = true;
-//		}
-//	}
 
 	//自ボールの場合，プロパティに参照を追加
 	if(datatype === "you") {
