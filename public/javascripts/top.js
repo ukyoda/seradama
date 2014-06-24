510eafde07b62ef64554c8ba40ec0cbeb146bfba
muff1225 <graphil1225@yahoo.co.jp>
2014-06-25 08:44:24 +0900

    tutorial作りかけ

--- public/javascripts/top.js
+++ public/javascripts/top.js
@@ -1,7 +1,11 @@
 ({
 	conf:{
+		msg:"aaaa, iiii, uuuu, eee",
+		i:1,
+		imax:2
 	},
 	init:function(){
+		var _conf = this.conf;
 		$(function(){
 			$('.start').click(function(e) {
 				$('.login-page').show("slow");
@@ -27,6 +31,35 @@
 				$('.login-index form').submit();
 				return false;
 			});
+			
+			$('.leftallow').click(function(e) {
+				if (_conf.i > 1) {
+					_conf.i -= 1;
+					this.tutorial(_conf.i);
+					// $('.tutrial-img').attr('src', "/images/tutorial" + _conf.i + ".png");
+					// $('#tutorial-msg').text(msg[_conf.i]);
+				} else {
+					_conf.i = _conf.imax;
+					$('.tutrial-img').attr('src', "/images/tutorial" + _conf.i + ".png");
+					$('#tutorial-msg').text(msg[_conf.i]);
+				}
+				return false;
+			});
+			$('.rightallow').click(function(e) {
+				if (_conf.i < _conf.imax) {
+				_conf.i += 1;
+				$('.tutrial-img').attr('src', "/images/tutorial" + _conf.i + ".png");
+				$('#tutorial-msg').text(msg[_conf.i]);
+				} else {
+					_conf.i = 1;
+					$('.tutrial-img').attr('src', "/images/tutorial" + _conf.i + ".png");
+				}
+				return false;
+			});
 		});
+	},
+	tutorial:function(i) {
+		$('.tutrial-img').attr('src', "/images/tutorial" + i + ".png");
+		$('#tutorial-msg').text(this.conf.msg.split(",")[i]);
 	}
 }).init();
