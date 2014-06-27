2f962fa58ee7b2e5fc2a6285dc9d663612565a6e
ukyoda <black12172001@hotmail.co.jp>
2014-06-28 01:55:27 +0900

    チュートリアルのJS修正
    + top.jsの５行目あたりの、msgsの配列にメッセージを追記して下さい。
    + メッセージに対応する画像を用意してください
      msgs[0] => tutorial1.png と対応
      msgs[1] => tutorial2.png と対応

--- public/javascripts/top.js
+++ public/javascripts/top.js
@@ -1,26 +1,59 @@
 ({
-	conf:{
-		msg:"aaaa, iiii, uuuu, eee",
-		i:1,
-		imax:2
+	conf: {},
+	tutorial:{
+		//画像の枚数分メッセージを表示して下さい。
+		msgs:[
+			"傾けるとボールが転がります",	//画像１番目のメッセージ
+			"複数人で遊べます",	//画像２番目のメッセージ
+			"エラー"	//画像３番目のメッセージ
+		],
+		current:0,
+		set: function(){
+			$('.tutrial-img').attr('src', this.getImgUrl());
+			$('#tutorial-msg').text(this.getMsg());
+			return this;
+		},
+		begin: function(){
+			this.current = 0;
+			return this;
+		},
+		prev: function(){
+			this.current = this.current < 0 ?this.msgs.length-1:--this.current;
+			return this;
+		},
+		next: function(){
+			this.current = this.current < (this.msgs.length-1) ?++this.current:0;
+			return this;
+		},
+		getMsg: function(){
+			return this.msgs[this.current];
+		},
+		getImgUrl: function(){
+			return "/images/tutorial" + (this.current+1) + ".png";
+		},
+		init:function(){
+			//画像読めなかったとき
+			$('.tutrial-img').get(0).onload=function(){
+				$(this).css('display', 'block');
+			};
+			//画像読めたとき
+			$('.tutrial-img').get(0).onerror=function(){
+				$(this).css('display', 'none');
+			};
+		}
 	},
 	init:function(){
-		var _conf = this.conf;
+		var that = this;
+		var _tutorial = this.tutorial;
 		$(function(){
+			_tutorial.init();
+			$('#tutorial-img').load(function(){});
 			$('.start').click(function(e) {
 				$('.login-page').show("slow");
 				return false;
 			});
 			$('.login-page .fa-times').click(function(e) {
 				$('.login-page').hide("slow");
-				return false;
-			});
-			$('.tutrial').click(function(e) {
-				$('.tutrial-page').show("slow");
-				return false;
-			});
-			$('.tutrial-page .fa-times').click(function(e) {
-				$('.tutrial-page').hide("slow");
 				return false;
 			});
 			$('.guest').click(function(e) {
@@ -31,35 +64,25 @@
 				$('.login-index form').submit();
 				return false;
 			});
-			
-			$('.leftallow').click(function(e) {
-				if (_conf.i > 1) {
-					_conf.i -= 1;
-					this.tutorial(_conf.i);
-					// $('.tutrial-img').attr('src', "/images/tutorial" + _conf.i + ".png");
-					// $('#tutorial-msg').text(msg[_conf.i]);
-				} else {
-					_conf.i = _conf.imax;
-					$('.tutrial-img').attr('src', "/images/tutorial_" + _conf.i + ".png");
-					$('#tutorial-msg').text(msg[_conf.i]);
-				}
+
+			$('.tutrial').click(function(e) {
+				e.preventDefault();
+				$('.tutrial-page').show("slow");
+				_tutorial.begin().set();
+			});
+			$('.tutrial-page .fa-times').click(function(e) {
+				e.preventDefault();
+				$('.tutrial-page').hide("slow");
 				return false;
+			});
+			$('.leftallow').click(function(e) {
+				e.preventDefault();
+				_tutorial.prev().set();
 			});
 			$('.rightallow').click(function(e) {
-				if (_conf.i < _conf.imax) {
-				_conf.i += 1;
-				$('.tutrial-img').attr('src', "/images/tutorial_" + _conf.i + ".png");
-				$('#tutorial-msg').text(msg[_conf.i]);
-				} else {
-					_conf.i = 1;
-					$('.tutrial-img').attr('src', "/images/tutorial_" + _conf.i + ".png");
-				}
-				return false;
+				e.preventDefault();
+				_tutorial.next().set();
 			});
 		});
-	},
-	tutorial:function(i) {
-		$('.tutrial-img').attr('src', "/images/tutorial_" + i + ".png");
-		$('#tutorial-msg').text(this.conf.msg.split(",")[i]);
 	}
 }).init();
