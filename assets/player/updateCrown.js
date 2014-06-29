c4b92f7cc734164ef694b5c41e3b32e59fc43bae
ukyoda <black12172001@hotmail.co.jp>
2014-06-29 13:46:38 +0900

    修正

--- assets/player/updateCrown.js
+++ assets/player/updateCrown.js
@@ -1,13 +1,12 @@
 
 Player.fn.updateCrown = function(rank){
 	rank = window.parseInt(rank, 10);
+	this.removeCrown();
 	if(rank === 1) {
 		this.setCrown('/texture/icons/gold.png');
 	} else if(rank === 2) {
 		this.setCrown('/texture/icons/silver.png');
 	} else if(rank === 3) {
 		this.setCrown('/texture/icons/blonze.png');
-	} else {
-		this.removeCrown();
 	}
 };
