a33de5e44f6994cef87a4862f102f329b9ee1544
ukyoda <black12172001@hotmail.co.jp>
2015-03-18 02:08:43 +0900

    model/objectsにあるモデルクラスを一括読み込みするモジュール

---
+++ model/GameObjects.js
@@ -0,0 +1,13 @@
+
+/**
+ * 各種ゲームのオブジェクトをまとめてrequireするためのやつ
+ */
+
+module.exports = {
+  Goal: require('./objects/Goal'),
+  Kabe: require('./objects/Kabe'),
+  Maru: require('./objects/Maru'),
+  Movable: require('./objects/Movable'),
+  MovableMaru: require('./objects/MovableMaru'),
+  Player: require('./objects/Player')
+};
