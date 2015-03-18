89d07a046d258ed89068177ed8e453f2348772dc
ukyoda <black12172001@hotmail.co.jp>
2015-03-19 02:22:07 +0900

    Stageオブジェクト移動により，パス修正

--- test/StageTest.js
+++ test/StageTest.js
@@ -1,17 +1,16 @@
 var funcs = require('./../model/GameFunctions');
-var Stage = require('./../model/Stage');
+var Stage = require('./../model/stage/Stage');
 var world = funcs.engine.createWorld(0,0,true);
 var stage = new Stage(world);
 var index;
 
 console.log(process.memoryUsage());
-console.log(world.DestroyBody.toString());
 stage.stageIndex = 20;
 //ステージを進めてもメモリリークは大丈夫そうか?
 for(index=0;index<10000;index++) {
-stage.nextStage();
+var data = stage.nextStage();
 if(index % 100 == 0) {
-  stage.destroyStage();
+  console.log(data);
   console.log(index,world.m_bodyCount, process.memoryUsage());
 }
 //console.log(stage.stageIndex, stage.stageName);
@@ -25,3 +24,4 @@ console.log(process.memoryUsage());
 setTimeout(function(){
   console.log(stage.getTime());
 }, 4000);
+console.log('Plesase wait 4000 ms...');
